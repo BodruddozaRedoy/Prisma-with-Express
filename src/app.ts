c709b0271a8bf18bcc54df5bf87e55afe6791ea1
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-09-27 12:55:20 +0600

    Updated prisma auth

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,40 @@
-import express from "express";
+import compression from "compression";
 import cors from "cors";
-import { userRouter } from "./modules/user/user.routes.js";
+import express from "express";
+import { userRouter } from "./modules/user/user.routes";
+import { authRouter } from "./modules/auth/auth.routes";
+import { postRouter } from "./modules/Post/post.routes";
 
 const app = express();
-app.use(cors());
-app.use(express.json())
+
+// Middleware
+app.use(cors()); // Enables Cross-Origin Resource Sharing
+app.use(compression()); // Compresses response bodies for faster delivery
+app.use(express.json()); // Parse incoming JSON requests
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/post", postRouter);
+app.use("/api/v1/auth", authRouter);
+
+// Default route for testing
+app.get("/", (_req, res) => {
+  res.send("API is running");
+});
 
 
-app.use("/api/v1/user", userRouter)
-
-app.get("/", (req, res) => {
-  res.send("Api is running");
+// 404 Handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
 });
 
 export default app;
